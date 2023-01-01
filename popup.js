ac73d7c5d62aed40ca7b27813fbd051a6637a7b7
Mohideen3800 <121677614+Mohideen3800@users.noreply.github.com>
2023-01-01 08:25:29 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,21 @@
-// Toggle the timer when the button is clicked
-document.getElementById("toggle-button").addEventListener("click", function() {
-  chrome.runtime.sendMessage({ type: "toggleTimer" });
+popup.js:
+let startTime;
+let endTime;
+
+// When the toggle button is clicked
+document.getElementById('toggle-button').addEventListener('click', function(event) {
+  // If the timer is not currently running
+  if (!startTime) {
+    // Start the timer
+    startTime = new Date();
+  } else {
+    // Stop the timer
+    endTime = new Date();
+    // Calculate the duration in seconds
+    const duration = (endTime - startTime) / 1000;
+    // Send a message to the background script with the duration
+    chrome.runtime.sendMessage({type: 'timeTracked', duration: duration});
+    // Reset the start time
+    startTime = null;
+  }
 });
